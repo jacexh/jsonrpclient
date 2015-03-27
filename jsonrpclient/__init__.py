6055d194210db86365c76ed3ab330438ecd70a16
Jace Xu <jace@xuh.me>
2015-03-27 23:15:00 +0800

    set version number

--- jsonrpclient/__init__.py
+++ jsonrpclient/__init__.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+__version__ = 0.1
+
 from .client import JsonRPCClient
 from .error import (MethodNotFound, InternalError, InvalidParamsError,
                     InvalidRequestError, ParseError, ServerError, ClientError)
